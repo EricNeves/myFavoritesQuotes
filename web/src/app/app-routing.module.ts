13c1a741a9e1e01aa2a4336256eb26f0a7edab59
Eric Neves <ericnevesrodrigues@gmail.com>
2024-02-10 18:56:59 -0400

    Feat: add new routes

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './components/views/home/home.component';
+import { SignupComponent } from './components/views/signup/signup.component';
+import { LoginComponent } from './components/views/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '',       component: HomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login',  component: LoginComponent }
 ];
 
 @NgModule({
