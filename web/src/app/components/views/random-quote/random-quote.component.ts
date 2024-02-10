b109d9625621ea80d5b15660180336c18b499431
Eric Neves <ericnevesrodrigues@gmail.com>
2024-02-10 17:52:57 -0400

    Feat: add random-quote component

---
+++ web/src/app/components/views/random-quote/random-quote.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random-quote',
+  templateUrl: './random-quote.component.html',
+  styleUrl: './random-quote.component.css'
+})
+export class RandomQuoteComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
+}
