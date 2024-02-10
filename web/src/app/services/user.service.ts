d1a9bea8a2b01abd0a3f2bb859cf50d950fc903f
Eric Neves <ericnevesrodrigues@gmail.com>
2024-02-10 19:24:19 -0400

    Feat: add user service

---
+++ web/src/app/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'
+
+import { User } from '../models/user.model';
+
+import { environment } from '../../environments/environment.development'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) { }
+
+  public register(user: User):Observable<User> {
+    return this.http.post<User>(`${environment.api}/users/create`, user);
+  }
+}
