292987657c109e737b486bfdde4afd76d70c8d11
Eric Neves <ericnevesrodrigues@gmail.com>
2024-02-10 18:56:18 -0400

    Feat: add login component

---
+++ web/src/app/components/views/login/login.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  public items: MenuItem[] = []
+
+  constructor() { }
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/'
+      },
+      {
+        label: 'Create Quote',
+        icon: 'pi pi-fw pi-plus',
+        items: [
+          {
+            label: 'Sign Up',
+            icon: 'pi pi-fw pi-user-plus',
+            routerLink: '/signup'
+          },
+          {
+            label: 'Login',
+            icon: 'pi pi-fw pi-sign-in',
+            routerLink: '/login'
+          }
+        ]
+      },
+      {
+        label: 'Contact',
+        icon: 'pi pi-fw pi-envelope',
+        items: [
+          {
+            label: 'Instagram',
+            icon: 'pi pi-fw pi-instagram',
+            url: 'https://www.instagram.com/ericneves_dev',
+            target: '_blank'
+          },
+          {
+            label: 'GitHub',
+            icon: 'pi pi-fw pi-github',
+            url: 'https://github.com/ericneves',
+            target: '_blank',
+          },
+          {
+            label: 'LinkedIn',
+            icon: 'pi pi-fw pi-linkedin',
+            url: 'https://www.linkedin.com/in/ericnevesrr',
+            target: '_blank'
+          }
+        ]
+      }
+    ]
+  }
+}
