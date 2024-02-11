be8e897bdb3a3335e5aba323726507effac26685
Eric Neves <ericnevesrodrigues@gmail.com>
2024-02-11 19:23:39 -0400

    Feat: add default data

--- init/tables.sql
+++ init/tables.sql
@@ -16,3 +16,15 @@ CREATE TABLE IF NOT EXISTS qt_quotes (
   user_id    INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES qt_users (id)
 );
+
+INSERT INTO qt_users (id, username, email, password, created_at) VALUES 
+(1, 'Richard',    'rich@email.com',      '$2y$10$2WcXBDEZO/mQh25xe8T5xel0Bf2kMy8ARkvZHuZP.Pbhhp2TpHcca', 'NOW()'),
+(2, 'Joe John',   'john@email.com',      '$2y$10$2WcXBDEZO/mQh25xe8T5xel0Bf2kMy8ARkvZHuZP.Pbhhp2TpHcca', 'NOW()'),
+(3, 'Elizabeth',  'elizabeth@email.com', '$2y$10$2WcXBDEZO/mQh25xe8T5xel0Bf2kMy8ARkvZHuZP.Pbhhp2TpHcca', 'NOW()');
+
+INSERT INTO qt_quotes (quote, author, created_at, user_id) VALUES 
+('Dê uma máscara ao homem e ele lhe dará a verdade!',      'Oscar Wilde',         'NOW()', 1),
+('Como não foi genial, não teve inimigos.',                'Oscar Wilde',         'NOW()', 2),
+('As máquinas me surpreendem muito frequentemente.',       'Alan Turing',         'NOW()', 2),
+('A falta de evidência não é uma evidência da ausência..', 'Carl Sagan',          'NOW()', 3),
+('Vence o medo e vencerás a morte.',                       'Alexandre, o Grande', 'NOW()', 1);
