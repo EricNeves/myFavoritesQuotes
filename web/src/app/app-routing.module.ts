16883e6f19d872e68d4dc641e8157edb05de7f28
Eric Neves <ericnevesrodrigues@gmail.com>
2024-02-11 19:34:47 -0400

    Feat: add user update page

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent      } from './components/views/home/home.component';
-import { SignupComponent    } from './components/views/signup/signup.component';
-import { LoginComponent     } from './components/views/login/login.component';
-import { DashboardComponent } from './components/views/dashboard/dashboard.component';
-import { CreateComponent    } from './components/views/dashboard/create/create.component';
-import { UpdateComponent    } from './components/views/dashboard/update/update.component';
+import { HomeComponent                 } from './components/views/home/home.component';
+import { SignupComponent               } from './components/views/signup/signup.component';
+import { LoginComponent                } from './components/views/login/login.component';
+import { DashboardComponent            } from './components/views/dashboard/dashboard.component';
+import { CreateComponent               } from './components/views/dashboard/create/create.component';
+import { UpdateComponent               } from './components/views/dashboard/update/update.component';
+import { UpdateComponent as UserUpdate } from './components/views/dashboard/user/update/update.component'
 
 import { authGuard } from './auth.guard';
 
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: 'dashboard',                  component: DashboardComponent, canActivate: [authGuard]},
   { path: 'dashboard/quote/create',     component: CreateComponent,    canActivate: [authGuard]},
   { path: 'dashboard/quote/:id/update', component: UpdateComponent,    canActivate: [authGuard]},
+  { path: 'dashboard/user/:id/update',  component: UserUpdate,         canActivate: [authGuard]}
 ];
 
 @NgModule({
